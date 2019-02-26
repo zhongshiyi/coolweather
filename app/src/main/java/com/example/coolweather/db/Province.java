c5aefb475f5f75658b32ec8d9047210c98264f23
zhongshiyi <1351896179@qq.com>
2019-02-26 16:19:35 +0800

    first commit.

---
+++ app/src/main/java/com/example/coolweather/db/Province.java
@@ -0,0 +1,27 @@
+package com.example.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport {
+    private int id;     //id是每个实体类都应该有的字段
+    private String provinceName;//省的名字
+    private int provinceCode;//记录省的代号
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public String getProvinceName(){
+        return provinceName;
+    }
+    public void setProvinceName(String provinceName){
+        this.provinceName = provinceName;
+    }
+    public int getProvinceCode(){
+        return provinceCode;
+    }
+    public void setProvinceCode(int provinceCode){
+        this.provinceCode = provinceCode;
+    }
+}
